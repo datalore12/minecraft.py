831022e198029a6d4155d073c4a1798adb438872
Jimmy <jfanizzi@gmail.com>
2016-01-09 04:00:12 +0000

    Some MC scripts

---
+++ hello.py
@@ -0,0 +1,8 @@
+Python 3.4.2 (default, Oct 19 2014, 13:31:11) 
+[GCC 4.9.1] on linux
+Type "copyright", "credits" or "license()" for more information.
+>>> 
+
+>>> from mcpi.minecraft import Minecraft
+>>> mc = Minecraft.create()
+>>> mc.postToChat("Hello World")
